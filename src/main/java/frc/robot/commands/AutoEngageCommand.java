3d0e1d5de78294dfd0bf04bbe2d31158a743657c
AnshulBompada <ancient.falme1145@gmail.com>
2023-10-08 06:27:29 -0500

    Changed PID and added auton arm requirements

--- src/main/java/frc/robot/commands/AutoEngageCommand.java
+++ src/main/java/frc/robot/commands/AutoEngageCommand.java
@@ -15,7 +15,7 @@ public class AutoEngageCommand extends ProfiledPIDCommand {
   public AutoEngageCommand(DriveSubsystem robotDrive) {
     super(
         new ProfiledPIDController(
-            0.023, //0.0234, //0.027
+            0.0235,// 0.023 //0.0234, //0.027
             0,
             0,
             new TrapezoidProfile.Constraints(1, 0.5)),
